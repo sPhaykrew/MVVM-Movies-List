a7d3a44d652e0d5ce4f1cf2c0023e93729de1aef
sPhaykrew <s_phaykrew@hotmail.com>
2022-06-10 17:14:48 +0700

    add progressBar

--- app/src/main/java/com/sakda/mvvm_movies_list/ui/main/view/MainFragment.kt
+++ app/src/main/java/com/sakda/mvvm_movies_list/ui/main/view/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -31,6 +32,18 @@ class MainFragment : Fragment() {
             recyclerViewAdapter.setItem(it.movies)
             fragmentMainBinding.recyclerview.adapter = recyclerViewAdapter
             recyclerViewAdapter.notifyDataSetChanged()
+        })
+
+        mainViewModel.errorMessage.observe(viewLifecycleOwner, Observer {
+            Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+        })
+
+        mainViewModel.loading.observe(viewLifecycleOwner, Observer {
+            if (it) {
+                fragmentMainBinding.progressBar.visibility = View.VISIBLE
+            } else {
+                fragmentMainBinding.progressBar.visibility = View.GONE
+            }
         })
 
         return fragmentMainBinding.root
--- app/src/main/java/com/sakda/mvvm_movies_list/ui/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/sakda/mvvm_movies_list/ui/main/viewmodel/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.sakda.mvvm_movies_list.ui.main.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -12,23 +11,32 @@ import kotlinx.coroutines.withContext
 
 class MainViewModel(private val repository: Repository) : ViewModel() {
 
-    val movies : MutableLiveData<MoviesList> = MutableLiveData()
+    val movies: MutableLiveData<MoviesList> = MutableLiveData()
+    val errorMessage: MutableLiveData<String> = MutableLiveData()
+    val loading: MutableLiveData<Boolean> = MutableLiveData()
 
     init {
         getMovies()
     }
 
-    private fun getMovies(){
-        viewModelScope.launch(Dispatchers.IO){
-           val response = repository.getMovies()
-            withContext(Dispatchers.Main){
-                if (response.isSuccessful){
+    private fun getMovies() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val response = repository.getMovies()
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
                     movies.postValue(response.body())
+                    loading.value = false
                 } else {
-                    Log.i("Error",response.message())
+                    onError("Error : ${response.message()} ")
                 }
             }
         }
     }
 
+    private fun onError(message: String) {
+        errorMessage.value = message
+        loading.value = false
+    }
+
 }
+
